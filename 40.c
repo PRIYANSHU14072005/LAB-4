0d9cc9a3249aa3a2db546cdc490f3e3e6649714a
PRIYANSHU14072005 <u24ai095@aid.svnit.ac.in>
2024-09-17 23:19:16 +0530

    Update 40.c

--- 40.c
+++ 40.c
@@ -1,1 +1,16 @@
 //READ NUMBERS TILL A NEGATIVE NUMBER IS ENTERED AND CALCULATE SUM OF A LIST OF NUMBERS READ
+#include <stdio.h>
+int main()
+{
+    int num, i, sum = 0;
+    while (num > 0)
+    {
+        printf("enter the number:");
+        scanf("%d", &num);
+        if (num < 0)
+            break;
+        sum += num;
+    }
+    printf("sum of positive number =%d", sum);
+    return 0;
+}
